03bd01ea18ccd684e1edf88beec9a48431e81705
Matteo <matteo.ferrone@gmail.com>
2023-09-27 22:00:32 +0200

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="row">
 
   <div class="col-12 col-md-6 mb-2">
-    <div class="card">
+    <div class="card shadow">
       <div class="card-header">
         Musica
       </div>
@@ -13,7 +13,7 @@
   </div>
 
   <div class="col-12 col-md-6 mb-2">
-    <div class="card">
+    <div class="card shadow">
       <div class="card-header">
         Films
       </div>
