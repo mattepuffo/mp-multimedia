d8278269c575ce10ceb26e9206bd9c6db2aef01e
Matteo <matteo.ferrone@gmail.com>
2023-09-27 21:55:48 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<div class="row">
+
+  <div class="col-12 col-md-6 mb-2">
+    <div class="card">
+      <div class="card-header">
+        Musica
+      </div>
+      <div class="card-body">
+        <h5 class="card-title mb-3">Special title treatment</h5>
+        <a href="/musica" class="btn btn-warning">Visualizza</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 mb-2">
+    <div class="card">
+      <div class="card-header">
+        Films
+      </div>
+      <div class="card-body">
+        <h5 class="card-title mb-3">Special title treatment</h5>
+        <a href="/films" class="btn btn-info">Visualizza</a>
+      </div>
+    </div>
+  </div>
+
+</div>
