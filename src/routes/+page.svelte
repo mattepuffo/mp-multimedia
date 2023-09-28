9e61395376258d7f07cde5480ccaf67fb53b765b
mattepuffo <matteo.ferrone@hotmail.it>
2023-09-28 12:48:55 +0200

    totali

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
+<script lang="ts">
+  import {onMount} from "svelte";
+
+  // let libri: any[] = [];
+
+  let cntMusica = 0;
+  let cntFilms = 0;
+
+  onMount(async () => {
+    const urlMusica = "https://www.matteoferrone.it/api/canzoni/get.php";
+    const respMusica = await fetch(urlMusica);
+    const dataMusica = await respMusica.json();
+    cntMusica = dataMusica.data.length;
+
+    const urlFilms = "https://www.matteoferrone.it/api/video/get.php";
+    const respFilms = await fetch(urlFilms);
+    const dataFilms = await respFilms.json();
+    cntFilms = dataFilms.data.length;
+  });
+</script>
+
 <div class="row">
 
   <div class="col-12 col-md-6 mb-2">
@@ -6,7 +27,7 @@
         Musica
       </div>
       <div class="card-body">
-        <h5 class="card-title mb-3">Special title treatment</h5>
+        <h5 class="card-title mb-3">Totale canzoni: {cntMusica}</h5>
         <a href="/musica" class="btn btn-warning">Visualizza</a>
       </div>
     </div>
@@ -18,7 +39,7 @@
         Films
       </div>
       <div class="card-body">
-        <h5 class="card-title mb-3">Special title treatment</h5>
+        <h5 class="card-title mb-3">Totale films: {cntFilms}</h5>
         <a href="/films" class="btn btn-info">Visualizza</a>
       </div>
     </div>
