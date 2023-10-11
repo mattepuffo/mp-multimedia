8519fbdec14e99bd8fd9ccad7ad2035e385077cd
mattepuffo <matteo.ferrone@hotmail.it>
2023-10-11 08:53:10 +0200

    online

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
       </div>
       <div class="card-body">
         <h5 class="card-title mb-3">Totale canzoni: {cntMusica}</h5>
-        <a href="/musica" class="btn btn-warning">Visualizza</a>
+        <a href="musica.html" class="btn btn-warning">Visualizza</a>
       </div>
     </div>
   </div>
@@ -41,7 +41,7 @@
       </div>
       <div class="card-body">
         <h5 class="card-title mb-3">Totale films: {cntFilms}</h5>
-        <a href="/films" class="btn btn-info">Visualizza</a>
+        <a href="films.html" class="btn btn-info">Visualizza</a>
       </div>
     </div>
   </div>
--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,7 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
             pages: 'build',
             assets: 'build',
             fallback: 'index.html',
@@ -22,6 +17,10 @@ const config = {
         }),
         appDir: '_app',
     }
+
+    // kit: {
+    //     adapter: adapter()
+    // }
 };
 
 export default config;
